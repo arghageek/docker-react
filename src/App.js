21b2dfbed4208cda1a1925fd7577f35a2847d019
Argha Das <arghadas@Arghas-MacBook-Air.local>
2022-12-13 20:10:02 +0530

    change app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Learning CICD
         </a>
       </header>
     </div>
